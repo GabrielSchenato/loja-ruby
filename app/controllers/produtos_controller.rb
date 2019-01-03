8a9eaf78540f3bf9e8dd335bf8d4704cab7a0a5a
Gabriel Schenato <gabriel.schenato@inovadora.com.br>
2019-01-03 21:01:55 -0200

    Criando a busca de produtos

--- app/controllers/produtos_controller.rb
+++ app/controllers/produtos_controller.rb
@@ -11,9 +11,14 @@ class ProdutosController < ApplicationController
     end
 
     def destroy
-    id = params[:id]
-    Produto.destroy id
-    redirect_to root_url
+        id = params[:id]
+        Produto.destroy id
+        redirect_to root_url
+    end
+
+    def busca
+        @nome = params[:nome]
+        @produtos = Produto.where "nome like ?",  "%#{@nome}%"
     end
 
 end
