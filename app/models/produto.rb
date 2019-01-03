f614105068d165fdbd66ba49f9e7267e57e34e52
Gabriel Schenato <gabriel.schenato@inovadora.com.br>
2019-01-03 21:25:24 -0200

    Tradução e Scaffold

--- app/models/produto.rb
+++ app/models/produto.rb
@@ -1,5 +1,8 @@
 class Produto < ApplicationRecord
 
+    validates :nome, length: {  minimum: 4  }
     validates :preco, presence: true
+    validates :descricao, length: { in: 5..200 }
+    validates :quantidade, numericality: { only_integer: true }
 
 end
